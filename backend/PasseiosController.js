d9bdd93e8ef7e6deda1187a1a91e766a80f13a26
Andressa Tancredi <andressa.tancredi@hotmail.com>
2019-08-18 16:47:34 -0300

    Cria Controller

---
+++ backend/PasseiosController.js
@@ -0,0 +1,9 @@
+const { connect } = require('./PasseiosRepository')
+const  {ClienteModel, PetModel, PasseadorSchema } = require('./PasseiosSchema')
+
+connect() //Conecta no mongoDB
+
+
+  module.exports = {
+
+  }
