19e3bc3fa3399b3a1d07893bb557188438bdd00c
Andressa Tancredi <andressa.tancredi@hotmail.com>
2019-08-23 17:09:38 -0300

    Add repositório de referencia

--- backend/PasseiosRepository.js
+++ backend/PasseiosRepository.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
-
 const MONGO_URL = "mongodb://localhost:27017/passeios";
+const MONGO_URL = process.env.MONGODB_URI;
 
 function connect () {
   mongoose.connect(MONGO_URL,
